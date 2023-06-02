9f7a55654285085a41bf02b022a99bdaea3bb62f
CarlynHumble <CHumble@spartaglobal.com>
2023-06-02 13:56:45 +0100

    Added changes to main

---
+++ Carlyn/src/main/java/com/sparta/cjh/Main.java
@@ -0,0 +1,8 @@
+package com.sparta.cjh;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Tech 215!");
+        System.out.println("You're all awesome!");
+    }
+}
--- GitCollab/src/main/java/org/example/Main.java
+++ GitCollab/src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Tech 215 are amazing!");
     }
 }
