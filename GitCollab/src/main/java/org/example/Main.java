979e97b0da8c33fb626238b3a9c08d039972bff4
Bellatrixxie <ljctozer@gmail.com>
2023-06-02 14:02:57 +0100

    message added

--- GitCollab/src/main/java/org/example/Main.java
+++ GitCollab/src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a = 6;
+        System.out.println("Git collab is fun!");
     }
 }
